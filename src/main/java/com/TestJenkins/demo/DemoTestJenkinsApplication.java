3639feda324594f4fa806de7c2d40738cbb2a110
anantchauhan953-maker <anantchauhan953@gmail.com>
2025-12-24 13:07:54 +0530

    TestJenking on Git Repov1

---
+++ src/main/java/com/TestJenkins/demo/DemoTestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.TestJenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestJenkinsApplication.class, args);
+	}
+
+}
